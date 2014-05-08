f3a2800092115cb9c5ceb5982e9ec257ce5c294b
Salvador RG Balestra <salrodgom@gmail.com>
2014-05-08 16:57:50 +0200

    erase and again

--- rings_properties.f90
+++
@@ -1,7 +0,0 @@
-SUBROUTINE xcryst2ring(n_atoms_in_ring,xcryst,
- IMPLICIT NONE
- INTEGER, intent(IN) :: n_atoms_in_ring
- INTEGER             :: i,j,k
- INTEGER             :: n_atoms
- REAL                :: xcrys_in_ring,xinbox_in_ring
-END SUBROUTINE xcryst2ring
